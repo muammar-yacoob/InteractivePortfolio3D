6380448f9259caa9b55a91ab3993f232872d0693
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-02-02 19:01:07 +0000

    Player moving around and animating using injected player input

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using SparkCore.Runtime.Core;
+using SparkCore.Runtime.Injection;
+using UnityEngine;
+
+namespace SparkGames.Portfolio3D
+{
+    [RequireComponent(typeof(Animator))]
+    public class PlayerAnimation : InjectableMonoBehaviour
+    {
+        [Inject] private IPlayerInput playerInput;
+        private Animator animator;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            var speed = playerInput.Movement.magnitude;
+            animator.SetFloat("Speed", speed);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SparkCore.Runtime.Injection;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SparkGames.Portfolio3D
+{
+    [ServiceProvider]
+    public class PlayerInput : IPlayerInput, IDisposable
+    {
+        private readonly PlayerControls playerControls;
+        private Vector2 movement;
+        public Vector2 Movement => movement;
+
+        public PlayerInput()
+        {
+            playerControls = new PlayerControls();
+            playerControls.GamePlay.Enable();
+            playerControls.GamePlay.Movement.performed += MovementPerformed;
+            playerControls.GamePlay.Movement.canceled += MovementCancelled;
+        }
+    
+        private void MovementPerformed(InputAction.CallbackContext ctx) => movement = ctx.ReadValue<Vector2>();
+        private void MovementCancelled(InputAction.CallbackContext ctx) => movement = Vector2.zero;
+
+        public void Dispose()
+        {
+            playerControls.GamePlay.Disable();
+            playerControls.GamePlay.Movement.performed -= MovementPerformed;
+            playerControls.GamePlay.Movement.canceled -= MovementCancelled;
+            playerControls?.Dispose();
+        }
+    }
+}
