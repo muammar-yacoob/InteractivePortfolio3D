9091acd9ebaa2d6e4a03271badb970cf7ff30f06
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-02-02 22:45:54 +0000

    tmp links work

---
+++ Assets/Scripts/UI/TMP_Links.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SparkGames.Portfolio3D.UI
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class TMP_Links : MonoBehaviour, IPointerClickHandler
+    {
+        private TMP_Text textUI;
+        private void Awake() => textUI = GetComponent<TMP_Text>();
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            int linkIndex = TMP_TextUtilities.FindIntersectingLink(textUI, Input.mousePosition, null);
+            if (linkIndex != -1)
+            {
+                TMP_LinkInfo linkInfo = textUI.textInfo.linkInfo[linkIndex];
+                Application.OpenURL(linkInfo.GetLinkID());
+            }
+        }
+    }
+}
