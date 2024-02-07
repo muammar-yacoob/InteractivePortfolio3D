0722c1cca870cd44c1905f37713b1ba8dcf29112
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-02-07 15:01:58 +0000

    camera culling works

---
+++ Assets/Scripts/ClearshotsCameraController.cs
@@ -0,0 +1,59 @@
+﻿using SparkCore.Runtime.Core;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace SparkGames.Portfolio3D
+{
+    public class ClearshotsCameraController : InjectableMonoBehaviour
+    {
+        [SerializeField] private CinemachineCamera freelookCam;
+        [SerializeField] private LayerMask playerLayer;
+
+        private int originalCullingMask;
+
+        private PrioritySettings highPriority;
+        private PrioritySettings lowPriority;
+        private Camera mainCam;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            highPriority = new PrioritySettings();
+            lowPriority = new PrioritySettings();
+            highPriority.Value = 100;
+            lowPriority.Value = 0;
+            mainCam = Camera.main;
+            originalCullingMask = mainCam.cullingMask; // Store the original culling mask.
+        }
+
+        private void OnEnable()
+        {
+            SubscribeEvent<WhiteboardEntered>(OnWhiteboardEntered);
+            SubscribeEvent<WhiteboardExited>(OnWhiteboardExited);
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeEvent<WhiteboardEntered>(OnWhiteboardEntered);
+            UnsubscribeEvent<WhiteboardExited>(OnWhiteboardExited);
+        }
+
+        private void OnWhiteboardEntered(WhiteboardEntered monoEvent)
+        {
+            Debug.Log("Entered whiteboard");
+            // Exclude the player layer for whiteboardCam by adjusting the main camera's culling mask directly using the playerLayer mask.
+            mainCam.cullingMask &= ~playerLayer.value;
+            freelookCam.Priority = lowPriority;
+            monoEvent.WhiteboardCam.Priority = highPriority;
+        }
+        
+        private void OnWhiteboardExited(WhiteboardExited monoEvent)
+        {
+            Debug.Log("Exited whiteboard");
+            // Reset the culling mask to include the player layer again.
+            mainCam.cullingMask = originalCullingMask;
+            freelookCam.Priority = highPriority;
+            monoEvent.WhiteboardCam.Priority = lowPriority;
+        }
+    }
+}
---
+++ Assets/Scripts/WhiteboardCameraController.cs
@@ -0,0 +1,50 @@
+﻿using SparkCore.Runtime.Core;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace SparkGames.Portfolio3D
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class WhiteboardCameraController : InjectableMonoBehaviour
+    {
+        [SerializeField] CinemachineCamera whiteboardCam;
+        protected override void Awake()
+        {
+            base.Awake();
+            GetComponent<BoxCollider>().isTrigger = true;
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out Player.PlayerMovement playerMovement))
+            {
+                PublishEvent(new WhiteboardEntered(whiteboardCam));
+            }
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent(out Player.PlayerMovement playerMovement))
+            {
+                PublishEvent(new WhiteboardExited(whiteboardCam));
+            }
+        }
+    }
+
+    public class WhiteboardEntered: MonoEvent
+    {
+        public CinemachineCamera WhiteboardCam { get; }
+        public WhiteboardEntered(CinemachineCamera whiteboardCam)
+        {
+            WhiteboardCam = whiteboardCam;
+        }
+    }
+    public class WhiteboardExited: MonoEvent
+    {
+        public CinemachineCamera WhiteboardCam { get; }
+        public WhiteboardExited(CinemachineCamera whiteboardCam)
+        {
+            WhiteboardCam = whiteboardCam;
+        }
+    }
+}
